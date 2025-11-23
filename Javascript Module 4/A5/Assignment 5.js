e71da7e9a4d1b9f64f18745f5e645c010b74c028
Aakash <aakashbasnet.info@gmail.com>
2025-11-23 22:01:23 +0200

    second push

---
+++ Javascript Module 4/A5/Assignment 5.js
@@ -0,0 +1,7 @@
+const url="https://api.chucknorris.io/jokes/random"
+    async function joke(){
+    const response=await fetch(url)
+        const json=await response.json()
+        console.log(json.value)
+    }
+    joke()
